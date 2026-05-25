33221b980c18e11b2a4a0680894853ab21f551b3
Someya Suleman <someyasuleman@gmail.com>
2026-05-25 16:51:14 +0530

    Added Project 1 - Color Switcher
    A color switcher if we click on any color then the whole body will changed to that color.
    So for this we use eventListener click and DOM selectors.

---
+++ colorSwitcher.js
@@ -0,0 +1,21 @@
+console.log("sumaiya");
+const buttons=document.querySelectorAll('.button')
+const body=document.querySelector("body")
+buttons.forEach(function(b){
+  b.addEventListener('click',function(e){
+    //console.log(e)
+    //console.log(e.target)
+    if(e.target.id==='grey'){
+      body.style.backgroundColor=e.target.id;
+    }
+    if(e.target.id==='white'){
+      body.style.backgroundColor=e.target.id;
+    }
+    if(e.target.id==='blue'){
+      body.style.backgroundColor=e.target.id;
+    }
+    if(e.target.id==='yellow'){
+      body.style.backgroundColor=e.target.id;
+    }
+  })
+})
